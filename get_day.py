f59a461989659b50b13bdd3501f98c8569ef4b00
takeshi tose <t.tose@uec.ac.jp>
2016-12-09 20:26:37 +0900

    新しいコンポーネントの追加 コンポーネント[get_day]を作成しました。

---
+++ get_day.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+#!/usr/bin/env python
+
+import re
+import datetime
+
+def get_day(text):
+	#datetimeの呼び出し
+	today = datetime.date.today()
+	one_day = datetime.timedelta(days=1)
+
+	day_today = re.search('今日|本日|これから',text)
+	if day_today :
+		return today.day
+
+	day_tomorrow =re.search('明日',text)
+	if day_tomorrow :
+		tomorrow = today + one_day
+		return tomorrow.day
