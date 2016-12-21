7e64ad52494d6c24833e084a27d7f36f6963a2c3
takeshi tose <t.tose@uec.ac.jp>
2016-12-21 15:20:29 +0900

    応答(一意)の生成_その２

--- test.py
+++ test.py
@@ -1,21 +1,36 @@
-
-'''
-if category_ans == 'what':
-	print('category is what')
-
-elif category_ans == 'when':
-	print('category is when')
-
-elif category_ans == 'who':
-	print('category is who')
-
-elif category_ans == 'where':
-	print('category is where')
+# coding: utf-8
+#!/usr/bin/env python
 
 
-elif category_ans == 'how':
-	print('category is how')
+ans0  ={'category' :'where',
+	   'what'      :'講演会',
+	   'where'     :'東3-501',
+	   'who'       :'西野教授',
+	   'when_time' :'13',
+	   'when_day'  :'17',
+	   'how'       :'3時間'}
 
-else:
-	print('category is why')
-'''
+ans1  ={'category' :'where',
+	   'what'      :'講演会',	
+	   'where'     :'講堂',
+	   'who'       :'高木教授',
+	   'when_time' :'13',
+	   'when_day'  :'17',
+	   'how'       :'1時間30分'}
+
+ans2  ={'category' :'where',
+	   'what'      :'講演会',
+	   'where'     :'東5-202',
+	   'who'       :'野田教授',
+	   'when_time' :'13',
+	   'when_day'  :'17',
+	   'how'       :'2時間'}
+
+
+#sum_of_ans = [ans0,ans1,ans2]
+#print(sum_of_ans)
+
+for anser in [ans0,ans1,ans2]:
+	print(anser['where'])
+	#print('----' + str(i) + '回目----')
+ 
